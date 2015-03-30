6cfcfe3aa6a77ff13768c49fd2598bb84f01212e
NathanLaan <nathanlaan@gmail.com>
2015-03-29 22:00:14 -0600

    Initial import. Functional but minimal.

---
+++ HDMonitor/AppForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace HDMonitor
+{
+    partial class AppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hdNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.contextMenuTitle = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hdNotifyIcon
+            // 
+            this.hdNotifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.hdNotifyIcon.BalloonTipText = "HD Monitor";
+            this.hdNotifyIcon.Text = "HD Monitor";
+            this.hdNotifyIcon.Visible = true;
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextMenuTitle,
+            this.contextMenuSeparator,
+            this.contextMenuExit});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(153, 76);
+            // 
+            // contextMenuExit
+            // 
+            this.contextMenuExit.Name = "contextMenuExit";
+            this.contextMenuExit.Size = new System.Drawing.Size(152, 22);
+            this.contextMenuExit.Text = "E&xit";
+            this.contextMenuExit.Click += new System.EventHandler(this.contextMenuExit_Click);
+            // 
+            // contextMenuSeparator
+            // 
+            this.contextMenuSeparator.Name = "contextMenuSeparator";
+            this.contextMenuSeparator.Size = new System.Drawing.Size(149, 6);
+            // 
+            // contextMenuTitle
+            // 
+            this.contextMenuTitle.Enabled = false;
+            this.contextMenuTitle.Name = "contextMenuTitle";
+            this.contextMenuTitle.Size = new System.Drawing.Size(152, 22);
+            this.contextMenuTitle.Text = "HD Monitor";
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AppForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "HD Monitor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon hdNotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuExit;
+        private System.Windows.Forms.ToolStripSeparator contextMenuSeparator;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuTitle;
+    }
+}
+
---
+++ HDMonitor/AppForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.Management;
+using System.Management.Instrumentation;
+
+namespace HDMonitor
+{
+    public partial class AppForm : Form
+    {
+
+        private const string MANAGEMENT_OBJECT_DISK_READ = "DiskBytesPersec";
+
+        private Icon hdIconOn;
+        private Icon hdIconOff;
+
+        private bool hdMonitorThreadRunning = false;
+        private Thread hdMonitorThread;
+
+
+        public AppForm()
+        {
+            InitializeComponent();
+
+            this.hdIconOn = new Icon("HD-ON.ico");
+            this.hdIconOff = new Icon("HD-OFF.ico");
+            this.hdNotifyIcon.Icon = this.hdIconOff;
+            this.hdNotifyIcon.ContextMenuStrip = this.contextMenu;
+
+            Application.ApplicationExit += Application_ApplicationExit;
+
+            this.hdMonitorThreadRunning = true;
+            this.hdMonitorThread = new Thread(new ThreadStart(this.hdMonitorThreadExecute));
+            this.hdMonitorThread.Start();
+        }
+
+
+        private void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            this.hdMonitorThreadRunning = false;
+            this.hdMonitorThread.Abort();
+            this.hdNotifyIcon.Dispose();
+            //MessageBox.Show("Exit");
+        }
+
+        private void contextMenuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void hdMonitorThreadExecute()
+        {
+            using (ManagementClass driveDataClass = new ManagementClass("Win32_PerfFormattedData_PerfDisk_PhysicalDisk"))
+            {
+                ManagementObjectCollection driveDataClassList = driveDataClass.GetInstances();
+                try
+                {
+                    while (this.hdMonitorThreadRunning)
+                    {
+                        driveDataClassList = driveDataClass.GetInstances();
+                        foreach (ManagementObject managementObject in driveDataClassList)
+                        {
+                            // We only want the "_Total" HD activity
+                            if (managementObject["Name"].ToString() == "_Total")
+                            {
+                                if (Convert.ToUInt64(managementObject[MANAGEMENT_OBJECT_DISK_READ]) > 0)
+                                {
+                                    this.hdNotifyIcon.Icon = hdIconOn;
+                                }
+                                else
+                                {
+                                    this.hdNotifyIcon.Icon = hdIconOff;
+                                }
+                            }
+                        }
+
+                        Thread.Sleep(10);
+                    }
+                }
+                catch (ThreadAbortException exception)
+                {
+                }
+                finally
+                {
+                    driveDataClassList.Dispose();
+                }
+            }
+
+        }
+
+
+    }
+}
